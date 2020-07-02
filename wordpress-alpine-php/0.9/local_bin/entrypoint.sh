73c8428d44adf8171c90443bfa01549982ed3cd3
Jenny Lawrance <jennylaw@microsoft.com>
2020-07-01 17:30:48 -0700

    Changing nginx log directory

--- wordpress-alpine-php/0.9/local_bin/entrypoint.sh
+++ wordpress-alpine-php/0.9/local_bin/entrypoint.sh
@@ -187,7 +187,7 @@ if [ ! $AZURE_DETECTED ]; then
 fi 
 
 test ! -d "$SUPERVISOR_LOG_DIR" && echo "INFO: $SUPERVISOR_LOG_DIR not found. creating ..." && mkdir -p "$SUPERVISOR_LOG_DIR"
-test ! -d "$NGINX_LOG_DIR" && echo "INFO: Log folder for nginx/php not found. creating..." && mkdir -p "$NGINX_LOG_DIR"
+test ! -d "$NGINX_LOG_DIR" && echo "INFO: Log folder for $NGINX_LOG_DIR not found. creating..." && mkdir -p "$NGINX_LOG_DIR"
 test ! -e /home/50x.html && echo "INFO: 50x file not found. createing..." && cp /usr/share/nginx/html/50x.html /home/50x.html
 # test -d "/home/etc/nginx" && mv /etc/nginx /etc/nginx-bak && ln -s /home/etc/nginx /etc/nginx
 # test ! -d "home/etc/nginx" && mkdir -p /home/etc && mv /etc/nginx /home/etc/nginx && ln -s /home/etc/nginx /etc/nginx
